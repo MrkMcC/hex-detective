92ca712da7034be4884023a02c65a1a9416a016b
Mark McCormack <SuddenLucidity@gmail.com>
2025-05-20 13:32:47 +0200

    untracked file

---
+++ src/types/SessionDataT.ts
@@ -0,0 +1,6 @@
+/**Information relating to the current play session. Initialised on new game, never used in the main menu.*/
+type SessionDataT = {
+  roundsWon: number;
+};
+
+export default SessionDataT;
