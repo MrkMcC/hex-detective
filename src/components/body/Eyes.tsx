95a160774a2b6cf49fb589f50a48e0df9d022f77
Mark McCormack <SuddenLucidity@gmail.com>
2025-06-12 12:43:22 +0200

    refactored - moved style files to their corresponding components

--- src/components/body/Eyes.tsx
+++ src/components/body/Eyes.tsx
@@ -7,29 +7,29 @@ interface Props {
 }
 
 const Eyes = ({ person }: Props) => {
-  const handleMouseMove = (e: MouseEvent) => {
-    var eyeL = document.querySelector<HTMLElement>(
-      `#person_${person.id} .eye.left .pupil-container`
-    )!;
-    var eyeR = document.querySelector<HTMLElement>(
-      `#person_${person.id} .eye.right .pupil-container`
-    )!;
-    const rect = eyeL.getBoundingClientRect();
-    const x = rect.left + (rect.right - rect.left) / 2;
-    const y = rect.top + (rect.bottom - rect.top) / 2;
-
-    var radL = Math.atan2(e.clientX - x, e.clientY - y);
-    var rotL = radL * (180 / Math.PI) * -1 + 180;
-    var radR = Math.atan2(e.clientX - x - 25, e.clientY - y);
-    var rotR = radR * (180 / Math.PI) * -1 + 180;
-    eyeL.style.transform = "rotate(" + rotL + "deg)";
-    eyeR.style.transform = "rotate(" + rotR + "deg)";
-  };
-
   useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const eyeL = document.querySelector<HTMLElement>(
+        `#person_${person.id} .eye.left .pupil-container`
+      )!;
+      const eyeR = document.querySelector<HTMLElement>(
+        `#person_${person.id} .eye.right .pupil-container`
+      )!;
+      const rect = eyeL.getBoundingClientRect();
+      const x = rect.left + (rect.right - rect.left) / 2;
+      const y = rect.top + (rect.bottom - rect.top) / 2;
+
+      const radL = Math.atan2(e.clientX - x, e.clientY - y);
+      const rotL = radL * (180 / Math.PI) * -1 + 180;
+      const radR = Math.atan2(e.clientX - x - 25, e.clientY - y);
+      const rotR = radR * (180 / Math.PI) * -1 + 180;
+      eyeL.style.transform = "rotate(" + rotL + "deg)";
+      eyeR.style.transform = "rotate(" + rotR + "deg)";
+    };
+
     document.addEventListener("mousemove", handleMouseMove);
     return () => document.removeEventListener("mousemove", handleMouseMove);
-  }, []);
+  }, [person.id]);
 
   return (
     <div className="eyes-container">
